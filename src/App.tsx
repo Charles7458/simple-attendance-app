d279d8c5db1c8cdbd8aef086453c552f15c5fcf2
Charles7458 <c.charles8547@gmail.com>
2025-09-10 21:19:11 +0530

    added clear button

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,14 @@ import { useState } from 'react'
 
 import './App.css'
 
-function Student(student : { name: string, id : number, setAttendance : (id: number,isPresent:boolean) => void}) {
+function Student(student : { name: string, id : number, attendance:boolean, setAttendance : (id: number,isPresent:boolean) => void}) {
 function handleCheck(checked:boolean) {
     student.setAttendance(student.id,checked);
     console.log(student.id, checked)
 }
   return (
     <div>
-      <input type="checkbox" onChange={e => handleCheck(e.target.checked)} />
+      <input type="checkbox" checked={student.attendance} onChange={e => handleCheck(e.target.checked)} />
       <span>{student.name}</span>
     </div>
   );
@@ -18,27 +18,37 @@ function handleCheck(checked:boolean) {
 function App() {
   const students = ["Charles", "Ammaar", "Poovarasan","Goushik","Mukilvannan", "Sameer", "Raghav", "Nithish", "Krishna", "Paari", 
     "Selvakumar","Arun", "Kaarmukil","Rawther"]
-  const [attendance,setAttendance] = useState(Array(students.length).fill(0));
+  const [attendance,setAttendance] = useState(Array(students.length).fill(false));
   console.log(attendance)
   const [total,setTotal] = useState(0);
+
   function handleAttendance(id:number,isPresent:boolean){
     const newAttendance = attendance;
     if(isPresent){
-      newAttendance[id] = 1;
+      newAttendance[id] = true;
     }
     else{
-      newAttendance[id] = 0;
+      newAttendance[id] = false;
     }
     setAttendance(newAttendance)
-    setTotal(n=>attendance.reduce((accumulator:number,curr:number)=>accumulator+curr,0))
+    let count = 0;
+    attendance.forEach(present=>{if(present){count++}})
+    setTotal(count)
     console.log(total)
+  }
+
+  function clear() {
+    setAttendance(Array(students.length).fill(false))
   }
 
   return (
     <div>
       <h1 className='head'>MCA Boys Attendance</h1>
-      <p>{total == attendance.length ? "All" : total} boys are present today...</p>
-      {students.map((student,i) => <Student name={student} id={i} setAttendance={handleAttendance} />)}
+      <div style={{display:"flex", alignItems:'center'}}>
+        <p>{total == attendance.length ? "All" : total} boys are present today...</p>
+        <button onClick={clear} className='clear-btn'>Clear</button>
+      </div>
+      {students.map((student,i) => <Student name={student} id={i} setAttendance={handleAttendance} attendance={attendance[i]} />)}
     </div>
   )
 }
